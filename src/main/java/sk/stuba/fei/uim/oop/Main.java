7b9e458561e65278c044365a00f57fd867ead74c
Matus Matuska <xmatuskam1@stuba.sk>
2022-03-21 21:21:02 +0100

    getter&setter

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -6,15 +6,13 @@ public class Main {
 
     public static void main(String[] args) {
         Auto skoda = new Auto();
-        Auto bmw = new Auto();
 
-        skoda.jedna = 1;
-        bmw.jedna = 2;
+        skoda.setStavNadrze(50);
 
-        if (skoda.jedna == bmw.jedna){
-            System.out.println("rovnake");
-        }else {
-            System.out.println("rozne");
-        }
+        skoda.jazdi(100);
+        System.out.println(skoda.getStavNadrze());
+
+        skoda.jazdi(200);
+        System.out.println(skoda.getStavNadrze());
     }
 }
--- src/main/java/sk/stuba/fei/uim/oop/auto/Auto.java
+++ src/main/java/sk/stuba/fei/uim/oop/auto/Auto.java
@@ -1,14 +1,23 @@
 package sk.stuba.fei.uim.oop.auto;
 
 public class Auto {
-    int pocetKolies;
-    int pocetDveri;
-    String nazov;
 
-    public int jedna;
-    int dva;
-    protected int tri;
-    private  int styri;
+    private  double kapacitaNadrze;
+    private  double stavNadrze;
 
+    public void jazdi(double distance){
+        stavNadrze -= distance * 0.2;
+        if (stavNadrze < 0){
+            stavNadrze = 0;
+        }
+    }
+
+    public double getStavNadrze() {
+        return stavNadrze;
+    }
+
+    public void setStavNadrze(double stavNadrze) {
+        this.stavNadrze = stavNadrze;
+    }
 }
 
